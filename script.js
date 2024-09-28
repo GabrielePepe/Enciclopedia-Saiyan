40a39559e958c4cd2e2f5f1e1746f4b21e3252d7
GabrielePepe <pepe.gabriele2010@icloud.com>
2024-09-28 20:00:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('read-more-btn').addEventListener('click', function() {
+    window.location.href = '/Folders/DBAFvol1.html';
+});
+
+document.getElementById('read-more-btn-2').addEventListener('click', function() {
+    window.location.href = '/Folders/DBAFvol2.html';
+});
