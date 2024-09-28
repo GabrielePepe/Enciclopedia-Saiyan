f4238df98df90a16fc7c07eaaa7fe6d81aa89478
GabrielePepe <pepe.gabriele2010@icloud.com>
2024-09-28 23:01:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('read-more-btn').addEventListener('click', function() {
-    window.location.href = '/Folders/DBAFvol1.html';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Folders/DBAFvol1.html';
 });
 
 document.getElementById('read-more-btn-2').addEventListener('click', function() {
-    window.location.href = '/Folders/DBAFvol2.html';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Folders/DBAFvol2.html';
 });
