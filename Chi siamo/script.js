46eecf96ce1e797e16dde0c85020d8a0cb898b6c
GabrielePepe <pepe.gabriele2010@icloud.com>
2024-10-18 20:16:33 +0200

    Add files via upload

---
+++ Chi siamo/script.js
@@ -0,0 +1,31 @@
+// Riferimenti ai vari elementi
+const menuIcon = document.getElementById('menu-icon');
+const fullscreenMenu = document.getElementById('fullscreen-menu');
+const closeBtn = document.getElementById('close-btn');
+
+// Mostra il menù quando si clicca sull'icona del menù
+menuIcon.addEventListener('click', () => {
+    fullscreenMenu.classList.add('open');
+});
+
+// Nasconde il menù quando si clicca sul pulsante di chiusura
+closeBtn.addEventListener('click', () => {
+    fullscreenMenu.classList.remove('open');
+});
+
+// Menu navigation buttons
+document.getElementById('menu-btn1').addEventListener('click', () => {
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/';
+});
+
+document.getElementById('menu-btn2').addEventListener('click', () => {
+    window.location.href = '/about.html';
+});
+
+document.getElementById('menu-btn3').addEventListener('click', () => {
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Base-html%20archive/index-base.html';
+});
+
+document.getElementById('menu-btn4').addEventListener('click', () => {
+    window.location.href = '/contact.html';
+});
