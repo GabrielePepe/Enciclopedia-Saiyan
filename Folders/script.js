e3b56d8971d77bcef9232ff250e023723ab80f80
GabrielePepe <pepe.gabriele2010@icloud.com>
2024-10-18 18:28:04 +0200

    Update script.js

--- Folders/script.js
+++ Folders/script.js
@@ -5,33 +5,27 @@ const closeBtn = document.getElementById('close-btn');
 
 // Mostra il menù quando si clicca sull'icona del menù
 menuIcon.addEventListener('click', () => {
-    alert("Hai cliccato sull'icona del menu!");  // Aggiunto alert per testare
     fullscreenMenu.classList.add('open');
 });
 
 // Nasconde il menù quando si clicca sul pulsante di chiusura
 closeBtn.addEventListener('click', () => {
-    alert("Hai cliccato sul pulsante di chiusura del menu!");  // Aggiunto alert per testare
     fullscreenMenu.classList.remove('open');
 });
 
 // Menu navigation buttons
 document.getElementById('menu-btn1').addEventListener('click', () => {
-    alert("Hai cliccato su 'Home'");  // Aggiunto alert per testare
     window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/';
 });
 
 document.getElementById('menu-btn2').addEventListener('click', () => {
-    alert("Hai cliccato su 'Chi siamo'");  // Aggiunto alert per testare
     window.location.href = '/about.html';
 });
 
 document.getElementById('menu-btn3').addEventListener('click', () => {
-    alert("Hai cliccato su 'Archivio base-html'");  // Aggiunto alert per testare
     window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Base-html%20archive/index-base.html';
 });
 
 document.getElementById('menu-btn4').addEventListener('click', () => {
-    alert("Hai cliccato su 'Contattaci'");  // Aggiunto alert per testare
     window.location.href = '/contact.html';
 });
