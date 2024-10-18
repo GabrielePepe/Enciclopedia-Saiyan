47d0c28b5ca32e66c044a55d83e0df5521ef5296
GabrielePepe <pepe.gabriele2010@icloud.com>
2024-10-18 20:33:22 +0200

    Update script.js

--- Chi siamo/script.js
+++ Chi siamo/script.js
@@ -15,17 +15,17 @@ closeBtn.addEventListener('click', () => {
 
 // Menu navigation buttons
 document.getElementById('menu-btn1').addEventListener('click', () => {
-    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/'; // Cambia il link con il percorso corretto
 });
 
 document.getElementById('menu-btn2').addEventListener('click', () => {
-    window.location.href = '/about.html';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Chi siamo/chi-siamo.html'; // Cambia il link con il percorso corretto
 });
 
 document.getElementById('menu-btn3').addEventListener('click', () => {
-    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Base-html%20archive/index-base.html';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Base-html archive/index-base.html'; // Cambia il link con il percorso corretto
 });
 
 document.getElementById('menu-btn4').addEventListener('click', () => {
-    window.location.href = '/contact.html';
+    window.location.href = 'https://gabrielepepe.github.io/Enciclopedia-Saiyan/Chi siamo/contact.html'; // Cambia il link con il percorso corretto
 });
